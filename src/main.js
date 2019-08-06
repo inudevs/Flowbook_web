1d0bb42019ae68f127bcfa7845395f616a29663d
mhibi <mhibio@naver.com>
2019-08-06 21:55:42 +0900

    main.js에 modal 추가

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-
+import VModal from 'vue-js-modal'
 import './style.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(VModal, { dynamic: true })
 
 /* eslint-disable no-new */
 new Vue({
